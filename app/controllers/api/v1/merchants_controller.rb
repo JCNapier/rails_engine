44dfe50f0b3251f5a1dcbef5093b6105e35d11b2
John Napier <jcee.napier@gmail.com>
2022-02-10 14:19:12 -0700

    Complete Item search action postman tests

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,13 @@ class Api::V1::MerchantsController < ApplicationController
   def show 
     render(json: MerchantSerializer.new(Merchant.find(params[:id])))
   end
+
+  def search 
+    merchant_list = Merchant.where("lower(name) like ?", "%#{params["name"].downcase}%").order(:name).limit(5)
+    if merchant_list.empty?
+      render(json: MerchantSerializer.new(merchant_list))
+    else 
+      render(json: MerchantSerializer.new(merchant_list))
+    end 
+  end
 end

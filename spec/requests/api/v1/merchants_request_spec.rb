f1f03bf3ca237c62a917888a4859d20b9f599a4c
John Napier <jcee.napier@gmail.com>
2022-02-10 18:06:46 -0700

    Move Merchant seach tests in to search spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -58,36 +58,36 @@ RSpec.describe 'the merchants API' do
     end
   end
 
-  it 'gets empty array for merchants case insensitive name search' do 
-    get "/api/v1/merchants/find_all?name=#{"name"}"
+  # it 'gets empty array for merchants case insensitive name search' do 
+  #   get "/api/v1/merchants/find_all?name=#{"name"}"
 
-    merchant_json = JSON.parse(response.body, symbolize_names: true)
+  #   merchant_json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful 
-    expect(merchant_json[:data]).to be_a(Array)
-    expect(merchant_json[:data].empty?).to be(true)
-  end
+  #   expect(response).to be_successful 
+  #   expect(merchant_json[:data]).to be_a(Array)
+  #   expect(merchant_json[:data].empty?).to be(true)
+  # end
 
-  it 'can get merchants for case insensitive name search' do
-    create(:merchant, name: 'Largemouth Bass')
-    create(:merchant, name: 'Smallmouth Bass')
-    create(:merchant, name: 'White Bass')
-    create(:merchant, name: 'Striped Bass')
-    create(:merchant, name: 'Spotted Bass')
-    create(:merchant, name: 'Sea Bass')
-    create(:merchant, name: 'Channel Catfish')
+  # it 'can get merchants for case insensitive name search' do
+  #   create(:merchant, name: 'Largemouth Bass')
+  #   create(:merchant, name: 'Smallmouth Bass')
+  #   create(:merchant, name: 'White Bass')
+  #   create(:merchant, name: 'Striped Bass')
+  #   create(:merchant, name: 'Spotted Bass')
+  #   create(:merchant, name: 'Sea Bass')
+  #   create(:merchant, name: 'Channel Catfish')
 
-    get "/api/v1/merchants/find_all?name=#{"bAsS"}"
+  #   get "/api/v1/merchants/find_all?name=#{"bAsS"}"
     
-    merchant_json = JSON.parse(response.body, symbolize_names: true)
+  #   merchant_json = JSON.parse(response.body, symbolize_names: true)
   
-    merchant = merchant_json[:data]
+  #   merchant = merchant_json[:data]
 
-    expect(response).to be_successful 
+  #   expect(response).to be_successful 
     
-    expect(merchant).to be_a(Array)
-    expect(merchant.first[:attributes]).to be_a(Hash)
-    expect(merchant.first[:attributes]).to have_key(:name)
-    expect(merchant.first[:attributes][:name]).to eq("Largemouth Bass")
-  end
+  #   expect(merchant).to be_a(Array)
+  #   expect(merchant.first[:attributes]).to be_a(Hash)
+  #   expect(merchant.first[:attributes]).to have_key(:name)
+  #   expect(merchant.first[:attributes][:name]).to eq("Largemouth Bass")
+  # end
 end

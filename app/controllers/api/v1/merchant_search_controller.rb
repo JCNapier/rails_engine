d10cda493543b1e0653720c56e135b8e38d3e0b5
John Napier <jcee.napier@gmail.com>
2022-02-10 18:12:33 -0700

    move controller AR logic to model

--- app/controllers/api/v1/merchant_search_controller.rb
+++ app/controllers/api/v1/merchant_search_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MerchantSearchController < ApplicationController
   def search 
-    merchant_list = Merchant.where("lower(name) like ?", "%#{params["name"].downcase}%").order(:name).limit(5)
+    merchant_list = Merchant.find_by_name(params[:name])
     if merchant_list.empty?
       render(json: MerchantSerializer.new(merchant_list))
     else 
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,10 @@ class Merchant < ApplicationRecord
   has_many :items
 
   validates_presence_of :name
+
+  def self.find_by_name(name) 
+    where("lower(name) like ?", "%#{name.downcase}%")
+    .order(:name)
+    .limit(5)
+  end
 end
